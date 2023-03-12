a9a1c8fdc39801dbdd7ce6213e4d6df303a95ab1
Camyil-89 <zhukovkd@yandex.ru>
2023-03-12 16:29:09 +0300

    Добавьте файлы проекта.

---
+++ Sharing/ViewModels/Windows/Main/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharing.ViewModels.Windows.Main
+{
+
+	class MainWindowVM : Base.ViewModel.BaseViewModel
+	{
+		public MainWindowVM()
+		{
+			#region Commands
+			#endregion
+		}
+
+		#region Parametrs
+		#endregion
+
+		#region Commands
+		#endregion
+
+		#region Functions
+		#endregion
+	}
+}
